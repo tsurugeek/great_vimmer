0dd9fe570582e685c193f3910578934e8e1f5a3d
Junya Kemmochi <github@tsurugeek.com>
2019-02-13 04:43:49 +0900

    add creation of grade record

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+grade = Grade.create(title: 'Great Vimmer!!!', published: true, grade_num: 1)
